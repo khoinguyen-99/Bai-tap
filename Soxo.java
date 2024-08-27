d8aeab48551302cb5b7f48068420f03f15c3e89a
khoinguyen-99 <khoinguyen9924@gmail.com>
2024-08-27 08:50:22 +0700

    Add files via upload

---
+++ Soxo.java
@@ -0,0 +1,16 @@
+
+package soxo;
+
+import java.util.Random;
+
+
+public class Soxo {
+
+    
+    public static void main(String[] args) {
+        Random rand =new Random();
+        int r = rand.nextInt(100)+1;
+        System.out.println("con so trung thuong la :"+r);
+    }
+    
+}
